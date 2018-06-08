733027836ff98369567615dd2b6ae2d89f6f46f3
Yonatan Kreiner <yonatankreiner@gmail.com>
2018-06-08 19:37:44 +0300

    removed web sockets between client and api

--- index.js
+++ index.js
@@ -8,122 +8,104 @@ let WebSocket = require("ws");
 let {WebSocketClient} = require("./client/js/WebSocketClient.js");
 let {BootstrapStep}   = require("./client/js/BootstrapStep.js");
 
-let server = express()
-	.use(express.static("client"))
-	.listen(2018, function() {
-		console.log("whatsapp-web-reveng HTTP server listening on port 2018");
-	});
+let server = express();
 
+server.use(express.static("client"));
 
-let wss = new WebSocket.Server({ server });
+server.get('/test', async (req, res) => {
+	let result = await test();
+	res.send(result);
+});
+
+server.listen(2018, function() {
+	console.log("whatsapp-web-reveng HTTP server listening on port 2018");
+});
 
 let backendInfo = {
 	url: "ws://localhost:2020",
 	timeout: 10000
 };
 
-wss.on("connection", function(clientWebsocketRaw, req) {
-	let backendWebsocket = new WebSocketClient();
-	let clientWebsocket = new WebSocketClient().initializeFromRaw(clientWebsocketRaw, "api2client", {getOnMessageData: msg => new StringDecoder("utf-8").write(msg.data)});
-	clientWebsocket.send({ type: "connected" });
-	//clientWebsocket.onClose(() => backendWebsocket.disconnect());
+async function test() {
+	try {
+		let backendWebsocket = new WebSocketClient();
 
-	clientWebsocket.waitForMessage({
-		condition: obj => obj.from == "client"  &&  obj.type == "call"  &&  obj.command == "api-connectBackend",
-		keepWhenHit: true
-	}).then(clientCallRequest => {
-		if(backendWebsocket.isOpen)
+		if(backendWebsocket.isOpen){
 			return;
-		new BootstrapStep({
+		}
+
+		let backendResponse = await (new BootstrapStep({
 			websocket: backendWebsocket,
 			actor: websocket => {
 				websocket.initialize(backendInfo.url, "api2backend", {func: WebSocket, args: [{ perMessageDeflate: false }], getOnMessageData: msg => new StringDecoder("utf-8").write(msg.data)});
 				websocket.onClose(() => {
-					clientWebsocket.send({ type: "resource_gone", resource: "backend" });
+					throw { type: "resource_gone", resource: "backend" };
 				});
 			},
 			request: {
 				type: "waitForMessage",
 				condition: obj => obj.from == "backend"  &&  obj.type == "connected"
 			}
-		}).run(backendInfo.timeout).then(backendResponse => {
-			clientCallRequest.respond({ type: "resource_connected", resource: "backend" });
-		}).catch(reason => {
-			clientCallRequest.respond({ type: "error", reason: reason });
-		});
-	}).run();
+		}).run(backendInfo.timeout))
 
-	clientWebsocket.waitForMessage({
-		condition: obj => obj.from == "client"  &&  obj.type == "call"  &&  obj.command == "backend-connectWhatsApp",
-		keepWhenHit: true
-	}).then(clientCallRequest => {
+		console.log('api connected to backend');
+
 		if(!backendWebsocket.isOpen) {
-			clientCallRequest.respond({ type: "error", reason: "No backend connected." });
 			return;
 		}
-		new BootstrapStep({
+		backendResponse = await (new BootstrapStep({
 			websocket: backendWebsocket,
 			request: {
 				type: "call",
 				callArgs: { command: "backend-connectWhatsApp" },
 				successCondition: obj => obj.type == "resource_connected"  &&  obj.resource == "whatsapp"  &&  obj.resource_instance_id
 			}
-		}).run(backendInfo.timeout).then(backendResponse => {
-			backendWebsocket.activeWhatsAppInstanceId = backendResponse.data.resource_instance_id;
-			backendWebsocket.waitForMessage({
-				condition: obj => obj.type == "resource_gone"  &&  obj.resource == "whatsapp",
-				keepWhenHit: false
-			}).then(() => {
-				delete backendWebsocket.activeWhatsAppInstanceId;
-				clientWebsocket.send({ type: "resource_gone", resource: "whatsapp" });
-			});
-			clientCallRequest.respond({ type: "resource_connected", resource: "whatsapp" });
-		}).catch(reason => {
-			clientCallRequest.respond({ type: "error", reason: reason });
+		}).run(backendInfo.timeout));
+		
+		backendWebsocket.activeWhatsAppInstanceId = backendResponse.data.resource_instance_id;
+		backendWebsocket.waitForMessage({
+			condition: obj => obj.type == "resource_gone"  &&  obj.resource == "whatsapp",
+			keepWhenHit: false
+		}).then(() => {
+			delete backendWebsocket.activeWhatsAppInstanceId;
+			throw { type: "resource_gone", resource: "whatsapp" };
 		});
-	}).run();
 
-	clientWebsocket.waitForMessage({
-		condition: obj => obj.from == "client"  &&  obj.type == "call"  &&  obj.command == "backend-disconnectWhatsApp",
-		keepWhenHit: true
-	}).then(clientCallRequest => {
+		console.log('backend connected to whatsapp');
+		
 		if(!backendWebsocket.isOpen) {
-			clientCallRequest.respond({ type: "error", reason: "No backend connected." });
-			return;
+			throw { type: "error", reason: "No backend connected." };
 		}
-		new BootstrapStep({
-			websocket: backendWebsocket,
-			request: {
-				type: "call",
-				callArgs: { command: "backend-disconnectWhatsApp", whatsapp_instance_id: backendWebsocket.activeWhatsAppInstanceId },
-				successCondition: obj => obj.type == "resource_disconnected"  &&  obj.resource == "whatsapp"  &&  obj.resource_instance_id == backendWebsocket.activeWhatsAppInstanceId
-			}
-		}).run(backendInfo.timeout).then(backendResponse => {
-			clientCallRequest.respond({ type: "resource_disconnected", resource: "whatsapp" });
-		}).catch(reason => {
-			clientCallRequest.respond({ type: "error", reason: reason });
-		});
-	}).run();
 
-	clientWebsocket.waitForMessage({
-		condition: obj => obj.from == "client"  &&  obj.type == "call"  &&  obj.command == "backend-generateQRCode",
-		keepWhenHit: true
-	}).then(clientCallRequest => {
-		if(!backendWebsocket.isOpen) {
-			clientCallRequest.respond({ type: "error", reason: "No backend connected." });
-			return;
-		}
-		new BootstrapStep({
+		backendResponse = await (new BootstrapStep({
 			websocket: backendWebsocket,
 			request: {
 				type: "call",
 				callArgs: { command: "backend-generateQRCode", whatsapp_instance_id: backendWebsocket.activeWhatsAppInstanceId },
 				successCondition: obj => obj.from == "backend"  &&  obj.type == "generated_qr_code"  &&  obj.image  &&  obj.content
 			}
-		}).run(backendInfo.timeout).then(backendResponse => {
-			clientCallRequest.respond({ type: "generated_qr_code", image: backendResponse.data.image })
-		}).catch(reason => {
-			clientCallRequest.respond({ type: "error", reason: reason });
-		})
-	}).run();
-});
+		}).run(backendInfo.timeout));
+
+		return { type: "generated_qr_code", image: backendResponse.data.image };
+	} catch (error) {
+		return { type: "error", reason: error };
+	}
+}
+
+function disconnect() {
+	if(!backendWebsocket.isOpen) {
+		throw { type: "error", reason: "No backend connected." };
+	}
+
+	new BootstrapStep({
+		websocket: backendWebsocket,
+		request: {
+			type: "call",
+			callArgs: { command: "backend-disconnectWhatsApp", whatsapp_instance_id: backendWebsocket.activeWhatsAppInstanceId },
+			successCondition: obj => obj.type == "resource_disconnected"  &&  obj.resource == "whatsapp"  &&  obj.resource_instance_id == backendWebsocket.activeWhatsAppInstanceId
+		}
+	}).run(backendInfo.timeout)
+	.then(backendResponse => {
+		console.log({ type: "resource_disconnected", resource: "whatsapp" });
+	});
+}
