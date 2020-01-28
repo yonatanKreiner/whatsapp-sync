644a5cf95bb66b886ccb11349325b1bc3abb319a
yonatan <yonatanKreiner@gmail.com>
2020-01-28 08:51:05 +0200

    Update googleContacts.js

--- api/googleContacts.js
+++ api/googleContacts.js
@@ -13,7 +13,7 @@ const credentials = {
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
     token_uri: "https://accounts.google.com/o/oauth2/token",
     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_secret: "OKBleQniLAPT0KKJHs3ld7ZM",
+    client_secret: "CLIENT_SERCRET",
     redirect_uris: [process.env.REDIRECT || "http://localhost:2018/authorized"]
 }
 
