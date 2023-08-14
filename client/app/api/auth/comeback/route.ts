6bfbf233ca339375f5bef64582a337f777b37c44
ofirelarat <96ofir11@gmail.com>
2023-08-15 00:24:13 +0300

    remove

--- client/app/api/auth/comeback/route.ts
+++ client/app/api/auth/comeback/route.ts
@@ -1,5 +1,4 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { google } from 'googleapis';
 import { oauth2Client, credentials } from '../oauth-config';
 
 export async function GET(request: NextRequest) {
--- client/app/api/auth/register/route.ts
+++ client/app/api/auth/register/route.ts
@@ -1,13 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-import { google } from 'googleapis';
 import { oauth2Client, scopes } from '../oauth-config';
-
-const credentials = {
-    client_id: "524139804888-5pmtubbjouds99tioias0b6amipak52f.apps.googleusercontent.com",
-    client_secret: "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",
-    redirect_uri: process.env.REDIRECT || "http://localhost:3000/api/auth/comeback"
-}
 
 async function googleAuth() {
     const url = oauth2Client.generateAuthUrl({
