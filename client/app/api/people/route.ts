6338362b5457750dbadb9ff091d84cbcea442bad
ofirelarat <96ofir11@gmail.com>
2023-08-15 17:30:11 +0300

    start working on updates

--- client/app/api/people/route.ts
+++ client/app/api/people/route.ts
@@ -1,26 +1,37 @@
 import { NextRequest, NextResponse } from 'next/server'
-import {google} from 'googleapis';
+import {google, people_v1} from 'googleapis';
 
 const people = google.people('v1');
 
 export async function GET(request: NextRequest) {
-    const accessToken = request.cookies.get("client-token")?.value;
-    console.log(accessToken);
-    
-    const {
-        data: {connections},
-      } = await people.people.connections.list({
-        personFields: 'names',
-        resourceName: 'people/me',
-        pageSize: 10,
-        oauth_token: accessToken
-      });
-
-      console.log("\n\nUser's Connections:\n");
-      connections?.forEach((c: any) => console.log(c));
-
-
-    return new Response('succeed authenticate', {
-        status: 200,
+  const accessToken = request.cookies.get("client-token")?.value;
+  console.log(accessToken);
+  
+  const {
+      data: {connections},
+    } = await people.people.connections.list({
+      personFields: 'names,resourceName,phoneNumbers',
+      resourceName: 'people/me',
+      pageSize: 10,
+      oauth_token: accessToken
     });
+
+  console.log("User's Connection");
+
+  const res = NextResponse.json(connections, {status: 200});
+
+  return res;
+}
+
+export async function POST(request: NextRequest){
+  const accessToken = request.cookies.get("client-token")?.value;
+  const {contacts, newcontactsimages} = request.body;
+  
+  const image = null;
+  contacts.forEach((contact: any) => {
+    people.people.updateContactPhoto({resourceName: contact.resourceName,requestBody: {photoBytes: image}, access_token: accessToken})  
+  });
+  
+
+
 }
