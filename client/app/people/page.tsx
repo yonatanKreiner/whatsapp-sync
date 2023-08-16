09f9c7246cfa01ba39c4f490b22146a4a4faed2d
ofirelarat <96ofir11@gmail.com>
2023-08-16 19:44:45 +0300

    remove staff

--- client/app/people/page.tsx
+++ client/app/people/page.tsx
@@ -7,36 +7,6 @@ import { WhatappConnector } from '../components/whatsapp-connector';
 import { ContactsImportsPhotos } from '../components/contacts-imports';
 
 export default function Page() {
-    const [people, setPeople] = useState([]);
-
-    const getPeople = async () => {
-        const res = await fetch('/api/people');
-        const data = await res.json();
-        
-        setPeople(data.map((p: any) => ({
-            resourceNane: p.resourceName,
-            name: p.names[0].displayName,
-            phone: p.phoneNumbers[0].value
-        })));
-    }
-
-    const auth = async () => {
-        const res = await fetch('/api/auth/register');
-        console.log(res)
-
-        if (res.status == 401) {
-            window.document.location = res.headers.get("location")!;
-        } else {
-            console.log('logged in');
-            await getPeople();
-        }
-
-        return res;
-    }
-
-    useEffect(() => {
-        // auth();
-    }, [])
 
     return (
         <div>
