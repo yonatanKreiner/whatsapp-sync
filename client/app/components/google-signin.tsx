6ab87dbfbf06a4ecf8193ec53185f5684bc58c32
ofirelarat <96ofir11@gmail.com>
2023-08-23 13:17:34 +0300

    added animation

--- client/app/components/google-signin.tsx
+++ client/app/components/google-signin.tsx
@@ -1,37 +1,43 @@
+import Lottie from "lottie-react";
 import { useEffect, useState } from "react";
-
+import LoadingAnimation from '../../public/assets/animation_loading.json';
 
 export const SignIn = () => {
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const onClickSignIn = async () => {
+        setIsLoading(true);
         const res = await fetch('/api/auth/register');
         console.log(res)
 
-        if(res.status === 200){
+        if (res.status === 200) {
             setIsLoggedIn(true);
-        }else if(res.status === 401){
+        } else if (res.status === 401) {
             document.location = res.headers.get("location")!;
         }
+
+        setIsLoading(false);
 
         return res;
     }
 
     useEffect(() => {
-        if(document.URL.includes('succeed')){
+        if (document.URL.includes('succeed')) {
             setIsLoggedIn(true);
         }
     });
 
     return (
-        <div>
-            {!isLoggedIn ? 
-                <div>
-                    <button onClick={onClickSignIn}>
-                        Sign In to Google
-                    </button>
-                </div>
-                : <div>You already logged in</div>}
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+            {isLoading ? <Lottie style={{width:"15%",height:"15%"}}  animationData={LoadingAnimation} /> :
+                !isLoggedIn ?
+                    <div>
+                        <button onClick={onClickSignIn}>
+                            Sign In to Google
+                        </button>
+                    </div>
+                    : <div>You already logged in</div>}
         </div>
     );
 }
