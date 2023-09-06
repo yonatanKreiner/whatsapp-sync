cf9c78d253651e58f68f654a06dd84cfea09155c
ofirelarat <96ofir11@gmail.com>
2023-09-06 12:36:26 +0300

    added results checks

--- client/app/api/people/update/route.ts
+++ client/app/api/people/update/route.ts
@@ -14,23 +14,32 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({ result: 'failed' }, { status: 400 });
   }
 
-  contacts.filter((x: any) => x.imageURL && x.resourceName)
-    .forEach(async (contact: any) => {
-      const imageUrl = contact.imageURL;
-      const imageUrlData = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+  const updatePromises: Promise<any>[] = contacts.filter((x: any) => x.imageURL && x.resourceName)
+    .map((x: any) => updateContactPhoto(x, accessToken!));
 
-      const imageData: string = imageUrlData.data;
-      const buffer = Buffer.from(imageData, 'base64');
+  const updateResults = await Promise.all(updatePromises);
 
-      const res = await people.people.updateContactPhoto({
-        resourceName: contact.resourceName,
-        requestBody: {
-          photoBytes: buffer.toString('base64')
-        },
-        access_token: accessToken
-      });
-      console.log(res);
-    });
 
-  return NextResponse.json({ "result": "succeed" }, { status: 200 });
+  const isSucceed = updateResults.every(r => r.updateResult);
+
+  return isSucceed ? NextResponse.json({ "result": "succeed" }, { status: 200 }) : NextResponse.json({ "result": "failed" }, { status: 500 }) ;
+}
+
+const updateContactPhoto = async (contact: any, accessToken: string) => {
+  const imageUrl = contact.imageURL;
+  const imageUrlData = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+
+  const imageData: string = imageUrlData.data;
+  const buffer = Buffer.from(imageData, 'base64');
+
+  const res = await people.people.updateContactPhoto({
+    resourceName: contact.resourceName,
+    requestBody: {
+      photoBytes: buffer.toString('base64')
+    },
+    access_token: accessToken
+  });
+
+
+  return {resourceName: res.data.person?.resourceName, updateResult: res.status == 200}
 }
