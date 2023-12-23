5fa6082c1b2731d25b7385c18adec55f4d111d96
ofirelarat <96ofir11@gmail.com>
2023-12-23 17:14:13 +0200

    new timeline style

--- client/app/connect-photos/page.tsx
+++ client/app/connect-photos/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { useEffect, useState } from 'react';
-import MultiStep from 'react-multistep';
 
 import { SignIn } from '../components/google-signin';
 import { WhatappConnector } from '../components/whatsapp-connector';
@@ -8,10 +7,24 @@ import { ContactsImportsPhotos } from '../components/contacts-imports';
 import { GoogleConnector } from '../components/google-connector';
 import { PricingTiers } from '../components/pricing-tiers';
 
+import { Timeline } from '@mantine/core';
+import { FaGoogle, FaTelegramPlane, FaWhatsapp } from "react-icons/fa";
+import { RiContactsBook2Fill } from "react-icons/ri";
+import { IoMdLogIn } from "react-icons/io";
+
+import './page.css'
+
 export default function Page() {
     const [whatsappContactsData, setWhatsappContactsData] = useState<any[] | undefined>(undefined);
     const [googleContactsData, setGoogleContactsData] = useState<any[] | undefined>(undefined);
     const [activeStep, setActiveStep] = useState<number>(0);
+
+    const moveToNextStep = () => {
+        setActiveStep((prev) => prev+1)
+    }
+    useEffect(()=>{
+        console.log(activeStep)
+    }, [activeStep])
 
     const onLoadWhatsappContacts = (contacts: any[]) => {
         setActiveStep(4)
@@ -24,22 +37,24 @@ export default function Page() {
      }
 
     return (
-        <div>
-            <MultiStep activeStep={activeStep}>
-                <div title='Sign In'>
-                    <SignIn></SignIn>
-                </div>
-                <div title='Plan choosing'><PricingTiers></PricingTiers></div>
-                <div title='Connect to google contacts'>
-                    <GoogleConnector setGoogleContactsData={onLoadGoogleContacts}></GoogleConnector>
-                </div>
-                <div title='Connect to Whatsapp'>
-                    <WhatappConnector setWhatsappContactsData={onLoadWhatsappContacts}></WhatappConnector>
-                </div>
-                <div title='Update contacts'>
-                    <ContactsImportsPhotos googleContacts={googleContactsData} whatsappContacts={whatsappContactsData}></ContactsImportsPhotos>
-                </div>
-            </MultiStep>
+        <div className='container-timeline'>
+            <Timeline active={activeStep} lineWidth={5} bulletSize={32} color="teal">
+                <Timeline.Item bullet={<IoMdLogIn size={15} />} title="SignIn">
+                    {activeStep === 0 ? <SignIn moveToNextStep={moveToNextStep}/> : <></>}
+                </Timeline.Item>
+                <Timeline.Item bullet={<FaTelegramPlane  size={15} />} title="Plan Choosing">
+                    {activeStep === 1 ? <PricingTiers  moveToNextStep={moveToNextStep} /> : <></>}
+                </Timeline.Item>
+                <Timeline.Item bullet={<FaGoogle  size={15} />} title="Google Contacts">
+                    {activeStep === 2 ? <GoogleConnector setGoogleContactsData={onLoadGoogleContacts} /> : <></>}
+                </Timeline.Item>
+                <Timeline.Item bullet={<FaWhatsapp  size={15} />} lineVariant="dashed" title="Whatsapp SignIn">
+                    {activeStep === 3 ? <WhatappConnector setWhatsappContactsData={onLoadWhatsappContacts} /> : <></>}
+                </Timeline.Item>
+                <Timeline.Item bullet={<RiContactsBook2Fill  size={15} />} title="Update Contacts">
+                    {activeStep === 4 ? <ContactsImportsPhotos googleContacts={googleContactsData} whatsappContacts={whatsappContactsData} /> : <></>}
+                </Timeline.Item>
+            </Timeline>
         </div>
     )
 }
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,9 +1,15 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter, Raleway} from 'next/font/google'
+import { createTheme, MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
 
 const inter = Inter({ subsets: ['latin'] })
 const raleway = Raleway({subsets: ['latin'],display: 'swap'})
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -18,20 +24,22 @@ export default function RootLayout({
   return (
     <html lang="en" className={raleway.className}>
       <body>
-        <div className="navbar">
-            <div id="logo">
-                <img id="logo-image" src="./assets/logo.svg" />
-            </div>
-        </div>
-
-        <div style={{minHeight:'69.5vh'}}>{children}</div>
-
-        <footer>
-              <div>
-                  Not affiliated with WhatsApp and we don't host any of the WhatsApp profile pictures on this website, all rights belong to their respective owners.<br />
-                  2018 © WhatsAppSync
+        <MantineProvider theme={theme}>
+          <div className="navbar">
+              <div id="logo">
+                  <img id="logo-image" src="./assets/logo.svg" />
               </div>
-        </footer>
+          </div>
+
+          <div style={{minHeight:'69.5vh'}}>{children}</div>
+
+          <footer>
+                <div>
+                    Not affiliated with WhatsApp and we don't host any of the WhatsApp profile pictures on this website, all rights belong to their respective owners.<br />
+                    2018 © WhatsAppSync
+                </div>
+          </footer>
+        </MantineProvider>
       </body>
     </html>
   )
