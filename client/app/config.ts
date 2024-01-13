71a7240a241f6743828d864cfa1b7614d52c9cd1
Ofir Elarat <96ofir11@gmail.com>
2024-01-13 19:28:11 +0200

    Update config.ts

--- client/app/config.ts
+++ client/app/config.ts
@@ -1,3 +1,3 @@
 export const JWT_SECRET = 'shhhhh';
 export const APP_URL = process.env.APP_URL || ''
-export const WHATSAPP_SOCKET_SERVICE = process.env.WHATSAPP_SOCKET_SERVICE || 'localhost:5000'
+export const WHATSAPP_SOCKET_SERVICE = process.env.NEXT_PUBLIC_WHATSAPP_SOCKET_SERVICE || 'localhost:5000'
