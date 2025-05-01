50314e5a0cfc59690a91bf520d3bdcc53250b42e
ofirelarat <96ofir11@gmail.com>
2025-05-01 16:58:57 +0300

    try

--- client/app/services/cookie-service.ts
+++ client/app/services/cookie-service.ts
@@ -1,15 +1,20 @@
-export function getCookie(cname: string): string {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
+export function getCookie(cname: string): string|null {
+  if (!document) {
+    return null;
   }
+
+  let name = cname + "=";
+
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
